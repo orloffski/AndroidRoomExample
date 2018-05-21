7b96f0f08f235bf9092f4f15206da24e44946d16
MadCat <orloffski@gmail.com>
2018-05-22 01:55:35 +0300

    Rev 0.3 add ignore run code in UI

--- app/src/main/java/com/example/madcat/androidroomexample/App.java
+++ app/src/main/java/com/example/madcat/androidroomexample/App.java
@@ -16,6 +16,7 @@ public class App extends Application {
         super.onCreate();
         instance = this;
         database = Room.databaseBuilder(this, AppDatabase.class, "database")
+                .allowMainThreadQueries()
                 .build();
     }
 
