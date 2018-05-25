5f75b1456c20fcb90cf56d35415d8ba85365008d
MadCat <orloffski@gmail.com>
2018-05-25 11:24:47 +0300

    Rev 0.7 Change index
    on Entity description

--- app/src/main/java/com/example/madcat/androidroomexample/database/entities/Employee.java
+++ app/src/main/java/com/example/madcat/androidroomexample/database/entities/Employee.java
@@ -1,16 +1,15 @@
 package com.example.madcat.androidroomexample.database.entities;
 
-import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Index;
 import android.arch.persistence.room.PrimaryKey;
 
-@Entity
+@Entity(indices = {@Index("name")})
 public class Employee {
 
     @PrimaryKey(autoGenerate = true)
     public long id;
 
-    @ColumnInfo(index = true)
     public String name;
 
     public int salary;
